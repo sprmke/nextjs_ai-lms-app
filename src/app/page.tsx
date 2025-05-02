9b40ce7269285c7b99970db51a02cdd71ae4a50a
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-03 07:21:16 +0800

    Add UserButton to validate user auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-4xl font-bold">Hello World</h1>
       <Button variant="outline">Click me</Button>
+      <UserButton />
     </div>
   );
 }
