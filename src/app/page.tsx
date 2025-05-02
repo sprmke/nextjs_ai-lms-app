664ab4db6e0c308122aefa18060977787d081ef0
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-02 18:06:31 +0800

    Update app base font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
+      <h1 className="text-4xl font-bold">Hello World</h1>
       <Button variant="outline">Click me</Button>
     </div>
   );
