07c9f7090adf834f6185cc3fd9911ec9de48d3db
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-06 01:32:17 +0800

    Use welcome banner to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import WelcomeBanner from '@/app/dashboard/_components/WelcomeBanner';
 
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <WelcomeBanner />
+    </div>
+  );
 }
 
 export default DashboardPage;
