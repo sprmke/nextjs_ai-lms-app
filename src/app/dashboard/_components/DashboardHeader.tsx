21f896883c17d9be4000d0c4a59b0a4894703935
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-06 01:24:24 +0800

    Add clerk user button to header

--- src/app/dashboard/_components/DashboardHeader.tsx
+++ src/app/dashboard/_components/DashboardHeader.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { UserButton } from '@clerk/nextjs';
 
 function DashboardHeader() {
-  return <div>DashboardHeader</div>;
+  return (
+    <div className="p-5 shadow-md flex justify-end">
+      <div className="w-[28px] h-[28px] bg-slate-100 rounded-full flex items-center justify-center">
+        <UserButton />
+      </div>
+    </div>
+  );
 }
 
 export default DashboardHeader;
