a7d7f2f5e5f183b885e84adf05fc9754619f8bfc
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-05 18:51:36 +0800

    Transfer our createNewUser logic from Provider to inngest function

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,4 +1,8 @@
+import { eq } from 'drizzle-orm';
 import { inngest } from '@/inngest/client';
+
+import { db } from '@/app/config/db';
+import { USER_TABLE } from '@/app/config/schema';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
@@ -6,5 +10,54 @@ export const helloWorld = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep('wait-a-moment', '1s');
     return { message: `Hello ${event.data.email}!` };
+  }
+);
+
+export const createNewUser = inngest.createFunction(
+  { id: 'create-user' },
+  { event: 'user.create' },
+  async ({ event, step }) => {
+    // Step to check user and create one if not found on database
+    await step.run(
+      'Check user and create one if not found on database',
+      async () => {
+        const { user } = event.data ?? {};
+        const { username, firstName, emailAddresses } = user ?? {};
+        const [firstEmailAddress] = emailAddresses ?? [];
+        const email = firstEmailAddress?.emailAddress;
+
+        // Check if user already exists
+        const existingUsers = await db
+          .select()
+          .from(USER_TABLE)
+          .where(eq(USER_TABLE.email, email));
+
+        console.log('existingUsers::', existingUsers);
+
+        // If not, add to database
+        if (!existingUsers?.length) {
+          const newUser = await db
+            .insert(USER_TABLE)
+            .values({
+              email,
+              userName: username ?? firstName ?? 'Anonymous',
+            })
+            .returning({
+              id: USER_TABLE.id,
+              userName: USER_TABLE.userName,
+              email: USER_TABLE.email,
+              isMember: USER_TABLE.isMember,
+            });
+
+          console.log('newUser::', newUser);
+        }
+
+        return { message: 'Successfully created user' };
+      }
+    );
+
+    // Step to send welcome email notification
+
+    // Step to send email notification after 3 days of user creation
   }
 );
