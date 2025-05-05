be99a5d3c28f8c59f56b82aead2b41e11d534492
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-06 01:31:29 +0800

    Create welcome banner component

---
+++ src/app/dashboard/_components/WelcomeBanner.tsx
@@ -0,0 +1,17 @@
+import { currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
+import React from 'react';
+
+export default async function WelcomeBanner() {
+  const user = await currentUser();
+
+  return (
+    <div className="flex items-center gap-x-6 p-5 bg-blue-500 w-full text-white rounded-lg">
+      <Image src="/logo.svg" alt="logo" width={40} height={40} />
+      <div>
+        <h3 className="font-bold text-3xl">Hello, {user?.fullName}</h3>
+        <p className="text-lg">Welcome back! Let&apos;s continue learning!</p>
+      </div>
+    </div>
+  );
+}
