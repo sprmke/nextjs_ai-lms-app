b7e60733f4f19deb5775c53dfccb79a6af99b302
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-06 01:31:19 +0800

    Add padding to dashboard layout content

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
       </div>
       <div className="md:ml-64">
         <DashboardHeader />
-        <div>{children}</div>
+        <div className="p-10">{children}</div>
       </div>
     </div>
   );
