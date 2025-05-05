d0bb6b9b052a81026d20e4280142a4e4d0a987e1
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-05 18:51:03 +0800

    Create create-user API endpoint that calls our inngest event

---
+++ src/app/api/create-user/route.ts
@@ -0,0 +1,13 @@
+import { inngest } from '@/inngest/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  const { user } = await req.json();
+
+  const result = await inngest.send({
+    name: 'user.create',
+    data: { user },
+  });
+
+  return NextResponse.json({ result });
+}
