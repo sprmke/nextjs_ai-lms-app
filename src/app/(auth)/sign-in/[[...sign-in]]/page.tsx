a3496faceb1333cfe5c6336529991c4110ab56d7
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-07 09:16:13 +0800

    Improve page name for auth pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from '@clerk/nextjs';
 
-export default function Page() {
+export default function SignInPage() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <SignIn />
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from '@clerk/nextjs';
 
-export default function Page() {
+export default function SignUpPage() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <SignUp />
