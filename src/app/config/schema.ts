be0c2ef2a762251747083d0fb563efb9fd48396e
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-05-08 01:03:47 +0800

    Add study material table on db

--- src/app/config/schema.ts
+++ src/app/config/schema.ts
@@ -1,8 +1,19 @@
-import { boolean, pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { boolean, json, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 
-export const USER_TABLE = pgTable("users", {
+export const USERS_TABLE = pgTable('users', {
   id: serial().primaryKey(),
   userName: varchar().notNull(),
   email: varchar().notNull(),
   isMember: boolean().default(false),
+});
+
+export const STUDY_MATERIALS_TABLE = pgTable('study_materials', {
+  id: serial().primaryKey(),
+  courseId: varchar().notNull(),
+  courseType: varchar().notNull(),
+  topic: varchar().notNull(),
+  difficultyLevel: varchar().default('Easy'),
+  courseLayout: json().default({}),
+  createdBy: varchar().notNull(),
+  status: varchar().default('Generating'),
 });
