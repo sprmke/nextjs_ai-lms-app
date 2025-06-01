77a74cc598244065a0a0a4cc8ff2d5693ca83a34
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-06-01 21:22:00 +0800

    Add comment to inggest API route

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,6 +2,7 @@ import { serve } from 'inngest/next';
 import { inngest } from '@/inngest/client';
 import { helloWorld, createNewUser } from '@/inngest/functions';
 
+// Serve the inngest API
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [helloWorld, createNewUser],
