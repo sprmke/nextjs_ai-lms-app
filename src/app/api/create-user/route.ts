83d1391927dea94d7506b35e35e7a89a0d747725
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-06-01 21:21:48 +0800

    Add comment to create user API route

--- src/app/api/create-user/route.ts
+++ src/app/api/create-user/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(req: NextRequest) {
   const { user } = await req.json();
 
+  // Create new user
   const result = await inngest.send({
     name: 'user.create',
     data: { user },
