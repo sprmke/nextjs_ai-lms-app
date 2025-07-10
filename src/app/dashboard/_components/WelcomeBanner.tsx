290c070c041df400f86298c53ac765d357e9edb3
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-07-10 23:44:26 +0800

    Improve dashboard logo

--- src/app/dashboard/_components/WelcomeBanner.tsx
+++ src/app/dashboard/_components/WelcomeBanner.tsx
@@ -7,9 +7,15 @@ export default async function WelcomeBanner() {
 
   return (
     <div className="flex items-center gap-x-6 p-5 bg-blue-500 w-full text-white rounded-lg">
-      <Image src="/logo.svg" alt="logo" width={40} height={40} />
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width={50}
+        height={50}
+        className="bg-blue-100 p-2 rounded-lg"
+      />
       <div>
-        <h3 className="font-bold text-3xl">Hello, {user?.fullName}</h3>
+        <h3 className="font-bold text-3xl">Hello, {user?.fullName}!</h3>
         <p className="text-lg">Let&apos;s continue learning!</p>
       </div>
     </div>
